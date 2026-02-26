f403ac4182f48a8f971c5f168d701944474cc32b
jmjubaer <jmjubaer3927@gmail.com>
2026-02-26 23:00:06 +0600

    setup the server with ts, dotenv,pretter and eslint

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Stl Server is runner on port 5000");
+});
+
+export default app;
