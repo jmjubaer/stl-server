3f12282731f7c33fea7f46aa1b39af7375d229e6
jmjubaer <jmjubaer3927@gmail.com>
2026-02-27 23:13:47 +0600

    update globalErrorHandler and handle all type or error and make in same model

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,5 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import config from '../config';
+import { ZodError } from 'zod';
+import handleZodError from '../errors/HanldeZodError';
+import handleValidationError from '../errors/HandleValidationError';
+import handleCastError from '../errors/HandleCastError';
+import AppError from '../errors/AppError';
+import handleDuplicateError from '../errors/HandleDuplicateError';
 
 export const globalErrorHandler = (
   error: any,
@@ -9,17 +15,56 @@ export const globalErrorHandler = (
 ) => {
   let statusCode = error.statusCode || 500;
   let message = error.message || 'Something went wrong';
-  let errorSource = [
+  let errorSources = [
     {
       path: '',
       message: 'Something went wrong',
     },
   ];
 
+  if (error instanceof ZodError) {
+    const formattedError = handleZodError(error);
+    message = formattedError.message;
+    statusCode = formattedError.statusCode;
+    errorSources = formattedError.errorSources;
+  } else if (error?.name === 'ValidationError') {
+    const formattedError = handleValidationError(error);
+    message = formattedError.message;
+    statusCode = formattedError.statusCode;
+    errorSources = formattedError.errorSources;
+  } else if (error?.name === 'CastError') {
+    const formattedError = handleCastError(error);
+    message = formattedError.message;
+    statusCode = formattedError.statusCode;
+    errorSources = formattedError.errorSources;
+  } else if (error?.code === 11000) {
+    const formattedError = handleDuplicateError(error);
+    message = formattedError.message;
+    statusCode = formattedError.statusCode;
+    errorSources = formattedError.errorSources;
+  } else if (error instanceof AppError) {
+    statusCode = error.statusCode;
+    message = error.message;
+    errorSources = [
+      {
+        path: '',
+        message: error.message,
+      },
+    ];
+  } else if (error instanceof Error) {
+    message = error.message;
+    errorSources = [
+      {
+        path: '',
+        message: error.message,
+      },
+    ];
+  }
+
   res.status(statusCode).send({
     success: false,
     message,
-    errorSource,
+    errorSources,
     stack: config.NODE_ENV === 'Development' ? error.stack : '',
   });
 };
