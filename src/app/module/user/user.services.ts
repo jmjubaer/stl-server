795af8f4960c3dff975402c8ace8974d4d76e5af
jmjubaer <jmjubaer3927@gmail.com>
2026-03-03 23:27:43 +0600

    fix upateme api and prevent update email, passworod via this api

--- src/app/module/user/user.services.ts
+++ src/app/module/user/user.services.ts
@@ -10,8 +10,16 @@ const getMeFromDb = async (id: string) => {
   const result = await userModel.findById(id);
   return result;
 };
+
 const updateMeIntoDb = async (id: string, payload: Partial<TUser>) => {
-  const result = await userModel.findByIdAndUpdate(id, payload);
+  // remove password and email from payload if exist
+  delete payload.password;
+  delete payload.email;
+
+  const result = await userModel.findByIdAndUpdate(id, payload, {
+    new: true,
+    runValidators: true,
+  });
   return result;
 };
 
